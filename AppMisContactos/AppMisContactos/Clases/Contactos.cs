02f083e9c86cb3325078e5f88381731c717c5928
NaDisla <tejadanadiam@gmail.com>
2020-05-29 20:29:14 -0400

    Creando conexión a la BD.

---
+++ AppMisContactos/AppMisContactos/Clases/Contactos.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppMisContactos.Clases
+{
+    public class Contactos
+    {
+		private string nombre;
+
+		public string Nombre
+		{
+			get { return nombre; }
+			set { nombre = value; }
+		}
+
+		private string apellido;
+
+		public string Apellido
+		{
+			get { return apellido; }
+			set { apellido = value; }
+		}
+
+		private string correo;
+
+		public string Correo
+		{
+			get { return correo; }
+			set { correo = value; }
+		}
+
+		private string telefono;
+
+		public string Telefono
+		{
+			get { return telefono; }
+			set { telefono = value; }
+		}
+
+
+	}
+}
--- AppMisContactos/AppMisContactos/NuevoContactoPage.xaml.cs
+++ AppMisContactos/AppMisContactos/NuevoContactoPage.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using AppMisContactos.Clases;
+using SQLite;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -15,6 +17,23 @@ namespace AppMisContactos
         public NuevoContactoPage()
         {
             InitializeComponent();
+        }
+
+        private void btnGuardarContacto_Clicked(object sender, EventArgs e)
+        {
+            Contactos nuevoContacto = new Contactos()
+            {
+                Nombre = txtNombre.Text,
+                Apellido = txtApellido.Text,
+                Correo = txtCorreo.Text,
+                Telefono = txtTelefono.Text
+            };
+
+            using (var connection = new SQLiteConnection(App.dbRuta))
+            {
+                connection.CreateTable<Contactos>();
+                connection.Insert(nuevoContacto);
+            }
         }
     }
 }
